861fc094d18aa3e8ea7345dc3fb40d5fc36d4b3e
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2012-01-21 11:46:56 +0200

    Show teachers in UI

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,20 @@
+
 App = Em.Application.create();
 
 App.School = Em.Object.extend({});
+
+App.Studio = Em.Object.extend({
+   // Returns classes sorted by the day of week (asc)
+  classesByDow: function() {
+    this.classes.sort(function(a,b){return a.dayofweek > b.dayofweek});
+    return this.classes;
+  }.property()
+  
+});
+
+App.YogaClass = Em.Object.extend({
+
+});
 
 App.schoolsController = Em.ArrayController.create({
   content: [],
@@ -11,11 +25,32 @@ App.schoolsController = Em.ArrayController.create({
       url: '/api/hajk', // Only one school at the moment
       dataType: 'json',
       success: function(data) {
-        self.pushObject(App.School.create(data));
+        self.pushObject(self.createSchool(data));
       }
     });
+  },
+
+  createSchool: function(schoolJson) {
+    studioModels = schoolJson.studios.map(function(studio) {
+      classModels = studio.classes.map(function(yogaClass) {
+        return App.YogaClass.create(yogaClass);
+      });
+
+      studio.classes = classModels;
+
+      return App.Studio.create(studio); // Create studio
+    });
+
+    schoolJson.studios = studioModels;
+    return App.School.create(schoolJson); // Create school
   }
+});
+
+App.StudioView = Em.View.extend({
+  content: {},
+  templateName: "studio-table-view",
 
 });
 
+// Load the data. This also populates the views and produces the DOM.
 App.schoolsController.loadData();
