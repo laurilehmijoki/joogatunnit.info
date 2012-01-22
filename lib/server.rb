6bd52a9003f716ce404276880b696638da7167b8
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2012-01-22 12:57:52 +0200

    Sinatra: serve only from public

--- lib/server.rb
+++ lib/server.rb
@@ -4,11 +4,6 @@ require 'sinatra'
 set :static, true
 set :public_folder, 'public'
 
-get '/api/:file' do
-  content_type :json  
-  File.open("json/#{params[:file]}", 'rb').read
-end
-
 get '/' do
   File.read(File.join('public', 'index.html'))
 end
