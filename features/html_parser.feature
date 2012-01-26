b6ec31a8561af220f85d5c3801f453c015d1c470
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2012-01-26 21:14:33 +0200

    More tests for the parsers

--- features/html_parser.feature
+++ features/html_parser.feature
@@ -3,7 +3,11 @@ Feature: Parse yoga class schedules from school websites
   Scenario: Parse Helsingin astanga joogakoulu's classes
     When I run the parser for Helsingin astanga joogakoulu
     Then the resulting JSON contains 5 studios
+    And each studio has at least 1 weekly class
+    And each class has at least name or teacher and day of week
 
   Scenario: Parse Moola's classes 
     When I run the parser for Moola 
     Then the resulting JSON contains 1 studio
+    And the studio has at least 1 weekly class
+    And each class has at least name or teacher and day of week
--- features/step_definitions/parser.rb
+++ features/step_definitions/parser.rb
@@ -12,3 +12,22 @@ end
 Then /^the resulting JSON contains (\d+) studios?$/ do |arg0|
   raise("Parsing failed") unless @hash.fetch('studios').length == arg0.to_i
 end
+
+Then /^(?:each|the) studio has at least (\d+) weekly class$/ do |arg1|
+  raise("Parsing failed") unless @hash.fetch('studios').each{|studio| studio.fetch('classes').length >= arg1.to_i }
+end
+
+Then /^each class has at least name or teacher and day of week$/ do
+  @hash.fetch('studios').each{|studio|
+    studio.fetch('classes').each{|yogaclass|
+      name = yogaclass.fetch('name')
+      teacher = yogaclass.fetch('teacher')
+      if (name == nil || name.strip.length == 0) && (teacher == nil || teacher.strip.length == 0)
+        raise("Either name or teacher must exist")
+      end
+
+      dow = yogaclass.fetch('dayofweek')
+      raise("Day of week missing") if dow == nil
+    }
+  }
+end
